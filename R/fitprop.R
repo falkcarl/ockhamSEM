2c874d55dcf004f67c902a9ab0b495b3d06ceba7
falkcarl <carl.falk@mcgill.ca>
2020-06-29 17:35:57 -0400

    major update to docs

--- R/fitprop.R
+++ R/fitprop.R
@@ -2,23 +2,165 @@
 
 #' Run fit propensity analyses
 #'
-#' @param ... models to be fit to the same data of class lavaan
-#' @param fit.measure character vector that indicates which fit measure to extract from fitted models
-#' @param rmethod string indicating the type of random correlation generation approach. Choices are "mcmc" (default), "onion", and "clustergen". See details.
-#' @param reps number of random correlation matrices to generate and fit models to
-#' @param onlypos logical value indicating whether to generate only positive correlation matrices
-#' @param seed random number seed used by set.seed or parallel package
-#' @param mcmc.args named list of arguments that controls options for "mcmc" correlation matrix generation. See details.
-#' @param clustergen.args named list of arguments that controls generation of correlation matrices if "onion" or "clustergen" is used. See details.
-#' @param saveModel logical value indicating whether the to save fitted models for later examination
-#' @param saveR logical value indicating whether to save random correlation matrices
-#' @param cluster a cluster created by makeCluster() from the parallel package
-#' @return An object of class fitprop
+#' @param ... Models of class lavaan for which the user would like to compare fit propensity.
+#' @param fit.measure Character vector that indicates which fit measure to extract from fitted
+#'   models. Possible options include anything returned by \code{\link[lavaan]{fitMeasures}} from the lavaan
+#'   package when applied to fitted models.
+#' @param rmethod String indicating the type of random correlation generation approach.
+#'   Choices are \code{"mcmc"} (default), \code{"onion"}, and \code{"clustergen"}. See details.
+#' @param reps Number of random correlation matrices to generate for fit propensity analysis.
+#' @param onlypos Logical value indicating whether to generate correlation matrices. Note that if there
+#'   are many variables, generation of correlation matrices and fitting models to them will be very, very
+#'   computationally intensive.
+#'   with positive manifold (\code{TRUE}); i.e., only positive relationships among variables.
+#' @param seed Random number seed used by set.seed or parallel package.
+#' @param mcmc.args Named list of arguments that controls options for
+#'   \code{"mcmc"} correlation matrix generation. See details.
+#' @param clustergen.args Named list of arguments that controls generation of
+#'   correlation matrices if \code{"onion"} or \code{"clustergen"} is used. See details.
+#' @param saveModel Logical value indicating whether the to save fitted models
+#'   for later examination.
+#' @param saveR Logical value indicating whether to save randomly generated correlation matrices.
+#' @param cluster (Optional) A cluster created by \code{\link[parallel]{makeCluster}} from the parallel package. If
+#'   provided, computations will be parallelized as much as possible.
+#' @details Inspired by work by Preacher (2003, 2006) and Bonifay & Cai (2017),
+#'   this function performs three steps for analyses to assess the fit propensity of competing
+#'   structural equation models: 1. Randomly generate correlation (or covariance matrices);
+#'   2. Fit models to each correlation matrix; and 3. Save a indices that could be used for
+#'   evaluating model fit in subsequent summaries. Conceptually, models that exhibit better fit
+#'   to such randomly generated data may have better fit propensity, and are therefore potentially
+#'   less parsimonious.
+#'
+#'   Analyses are performed with the \code{lavaan} package, and fitted lavaan models of
+#'   \code{\link[lavaan]{lavaan-class}} (e.g., created from
+#'   \code{\link[lavaan]{cfa}}, \code{\link[lavaan]{sem}}, or \code{\link[lavaan]{lavaan}} functions)
+#'   for the competing models must be passed as initial arguments
+#'   to the function. Currently, only single-group models and those relying on ML estimation are
+#'   supported. Otherwise, the underlying options for the fitted lavaan models will be re-used by
+#'   the run.fitprop function for the fit propensity analyses. It is optional to save the randomly
+#'   generated matrices from Step 1 and the models fit in Step 2. Follow-up summaries of results
+#'   saved in Step 3 are provided by plot.fitprop and summary.fitprop functions.
+#'
+#'   Generation of random correlation matrices is provided using several approaches. The \code{"mcmc"}
+#'   algorithm implements a Markov Chain Monte Carlo approach and was ported from Fortran code
+#'   in Preacher (2003). For details on the algorithm's actual implementation, see Preacher (2003),
+#'   Falk and Muthukrishna (in prep), or the source code for the mcmc function. If this algorithm
+#'   is chosen, \code{mcmc.args} accepts a list that can modify some default settings. In particular,
+#'   \code{iter} sets the total number of iterations to run (default = 5000000). If parallel processing
+#'   is enabled, this number will be divided amonst the number of chains. \code{miniter} sets a
+#'   minimum number of iterations per chain to avoid many processors leading to too few iterations per
+#'   chain (default = 10000). \code{jmpsize} overrides the step size for each update to the candidate
+#'   correlation matrix. Smaller step sizes typically lead to more acceptance and may be necessary for
+#'   larger correlation matrices (default jump size depends on the number of variables). Though, in
+#'   general the MCMC algorithm becomes more difficult to work well with many variables.
+#'
+#'   The \code{"onion"} method is one approach that relies on work of Joe (2006) and
+#'   Lewandowski, Kurowick, and Joe (2009); matrices are generated recursively, one variable at a
+#'   time. The onion method is computationally more efficient than the MCMC algorithm. Under the
+#'   hood, the \code{\link[clusterGeneration]{genPositiveDefMat}} function in the clusterGeneration package is used, with default
+#'   arguments of \code{covMethod="onion"}, \code{eta=1}, and \code{rangeVar=c(1,1)}. These arguments ensure that the
+#'   Onion method is used, generation is uniform over the space of positive definite matrices
+#'   (but see note on positive manifold below), and with unit variances.
+#'
+#'   An additional option \code{"clustergen"} is provided for direct interface with the \code{\link[clusterGeneration]{genPositiveDefMat}}
+#'   function in the clusterGeneration package. A named list can be passed to \code{clustergen.args} to
+#'   override any defaults used by \code{\link[clusterGeneration]{genPositiveDefMat}}, and the user is referred to documentation
+#'   for that function. This allows, for example, generation using C-Vines, covariance matrices
+#'   (i.e., variables that do not all have unit variances), and several other covaraince/correlation
+#'   matrix generation techniques.
+#'
+#'   onlypos controls whether correlation matrices can have only positive correlations.
+#'   The original MCMC algorith by Preacher (2003, 2006) generated correlation matrices with
+#'   positive manifold only (i.e., only positive correlations). The algorithm is easily changed
+#'   to allow also negative correlations. The Onion method and any functions from clusterGeneration
+#'   by default generate matrices with both positive and negative correlations. To obtain
+#'   matrices with positive manifold only, an ad-hoc correction is implemented for these latter
+#'   approaches where the matrix is transformed: R = (R+1)/2. To our knowledge, there is no
+#'   guarantee that this will result in uniform sampling from the space of all correlation matrices
+#'   with positive manifold, yet fit propensity results for some examples are very similar to those
+#'   of the MCMC algorithm.
+#'
+#' @slot fit_list A list of the same length as the number of models being compared. Each list
+#'   contains a matrix with columns corresponding to each entry of \code{fit.measure} and for all replications.
+#' @slot R A list of the same length as the number of replications, containing all correlation
+#'   matrices that were used for the fit propensity analysis. This slot is only populated
+#'   if \code{SaveR} is set to \code{TRUE}.
+#' @slot mod_list A list of the same length as the number of models being compared. Each element
+#'   contains a list of the same length as the number of replications and contains fitted lavaan
+#'   models. Only populated if \code{saveModel} is set to \code{TRUE}.
+#' @references
+#' Bonifay, W. E., & Cai, L. (2017). On the complexity of item response theory models. Multivariate Behavioral Research, 52(4), 465–484. \url{http://doi.org/10.1080/00273171.2017.1309262}
+#'
+#' Falk, C. F., & Muthukrishna, M. (in preparation). Parsimony in model selection: Tools for assessing fit propensity.
+#'
+#' Lewandowski, D., Kurowicka, D., & Joe, H. (2009). Generating random correlation matrices based on vines and extended onion method. Journal of Multivariate Analysis,100(9), 1989–2001. \url{http://doi.org/10.1016/j.jmva.2009.04.008}
+#'
+#' Joe, H. (2006). Generating random correlation matrices based on partial correlations. Journal of Multivariate Analysis, 97(10), 2177–2189. \url{http://doi.org/10.1016/j.jmva.2005.05.010}
+#'
+#' Preacher, K. J. (2003). The role of model complexity in the evaluation of structural equation models (PhD thesis). The Ohio State University.
+#'
+#' Preacher, K. J. (2006). Quantifying parsimony in structural equation modeling. Multivariate Behavioral Research, 41(3), 227–259. \url{http://doi.org/10.1207/s15327906mbr4103_1}
+#' @examples
+#' \donttest{
+#' # Set up a covariance matrix to fit models to
+#' p<-3 # number of variables
+#' temp_mat <- diag(p) # identity matrix
+#' colnames(temp_mat) <- rownames(temp_mat) <- paste0("V", seq(1, p))
+#'
+#' # Define and fit two models using lavaan package
+#' mod1a <- 'V3 ~ V1 + V2
+#'   V1 ~~ 0*V2'
+#' mod2a <- 'V3 ~ V1
+#'   V2 ~ V3'
+#'
+#' mod1a.fit <- sem(mod1a, sample.cov=temp_mat, sample.nobs=500)
+#' mod2a.fit <- sem(mod2a, sample.cov=temp_mat, sample.nobs=500)
+#'
+#' # Run fit propensity analysis a variety of different ways
+#'
+#' # Onion approach, only positive correlation matrices, save srmr
+#' res <- run.fitprop(mod1a.fit, mod2a.fit, fit.measure="srmr",
+#'   rmethod="onion",reps=1000,onlypos=TRUE)
+#' summary(res)
+#'
+#' # Onion approach, save several fit indices
+#' res <- run.fitprop(mod1a.fit, mod2a.fit, fit.measure=c("srmr","cfi","rmsea"),
+#'   rmethod="onion",reps=1000)
+#' summary(res)
+#'
+#' # mcmc approach, with parallel processing (4 cores)
+#' # Save and then access correlation matrices and fitted models
+#' # Note: this will take a very long time as the default number
+#' # of iterations is set to a lot
+#' cl<-makeCluster(4)
+#' res <- run.fitprop(mod1a.fit, mod2a.fit, fit.measure="srmr",
+#'   rmethod="mcmc",reps=1000,cluster=cl,saveModel=TRUE,saveR=TRUE)
+#' stopCluster(cl)
+#' summary(res)
+#' res$R[[1]] # Correlation matrix for first replication
+#' res$fit_list[[1]] # Saved fit indices for first model
+#' res$mod_list[[1]][[2]] # Fitted lavaan model for first model, second replication
+#'
+#' # mcmc approach, overriding defaults
+#' ctrl<-list(
+#'   iter = 10000, # total number of iterations
+#'   miniter = 5000, # but, min number of iterations per chain
+#'   jmpsize = .3
+#' )
+#' cl<-makeCluster(4)
+#' res <- run.fitprop(mod1a.fit, mod2a.fit, fit.measure="srmr",
+#'   rmethod="mcmc",reps=1000,cluster=cl, mcmc.args=ctrl)
+#' stopCluster(cl)
+#' summary(res)
+#'
+#' }
+#' @return An object of class fitprop for which plot and summary methods are available. Some slots are listed in the Slots section.
+#' @seealso \code{\link[ockhamSEM]{plot.fitprop}} \code{\link[ockhamSEM]{summary.fitprop}}
 #' @export
 #' @importFrom lavaan lavInspect lavNames parTable lavaan
 #' @import parallel
 run.fitprop <- function(...,
-                        fit.measure=c("srmr"),
+                        fit.measure="srmr",
                         rmethod=c("onion","mcmc","clustergen"),
                         reps=1000,
                         onlypos=FALSE,
@@ -169,18 +311,63 @@ run.fitprop <- function(...,
 #' Plot function for fitprop objects
 #' @param x Object of class fitprop, as created by run.fitprop function.
 #' @param ... Does nothing but to hopefully make this generic function pass R CMD check
-#' @param type What type of plot to produce?
-#' @param whichmod Index number corresponding to which model(s) to include on the plot
-#' @param whichfit Character vector indicating which indices of model fit to include
+#' @param type What type of plot to produce? Options include \code{"ecdf"} or \code{"euler"}. \code{"nVennR"}
+#'   is not yet operational.
+#' @param whichmod Index number corresponding to which model(s) to include on the plot. Defaults to all models.
+#' @param whichfit Character vector indicating which indices of model fit to include. Defaults to all saved indices.
 #' @param savePlot Logical value indicating whether to save plot to a list (TRUE) or just produce plot to output.
-#' @param xlim Vector of length 2 indicating the limits for the x-axis of the plot
-#' @param samereps Logical value indicating whether to use only results from replications in which all selected models yielded results
-#' @param cutoff Numeric vector indicating what cut value of the fit indice(s) to use for euler plots
-#' @param lower.tail Logical vector indicating whether lower values of each fit index corresponds to good fit
-#' @param mod.lab Optional character vector of labels for each model
-#' @param mod.brewer.pal Optional character corresponding to the palette from RColorBrewer to use for the different models
+#' @param xlim Numeric vector of length 2 indicating the limits for the x-axis of the plot.
+#' @param samereps Logical value indicating whether to use only results from replications in which all selected models yielded results.
+#' @param cutoff Numeric vector indicating what cut value of the fit indice(s) to use for euler plots.
+#' @param lower.tail Logical vector indicating whether lower values of each fit index corresponds to good fit.
+#' @param mod.lab Optional character vector of labels for each model.
+#' @param mod.brewer.pal Optional string corresponding to the palette from RColorBrewer to use for the different models. e.g.,
+#'   see \code{\link[RColorBrewer]{display.brewer.all}}.
+#' @examples
+#' \donttest{
+#'
+#' # Set up a covariance matrix to fit models to
+#' p<-3 # number of variables
+#' temp_mat <- diag(p) # identity matrix
+#' colnames(temp_mat) <- rownames(temp_mat) <- paste0("V", seq(1, p))
+#'
+#' # Define and fit two models using lavaan package
+#' mod1a <- 'V3 ~ V1 + V2
+#'   V1 ~~ 0*V2'
+#' mod2a <- 'V3 ~ V1
+#'   V2 ~ V3'
+#'
+#' mod1a.fit <- sem(mod1a, sample.cov=temp_mat, sample.nobs=500)
+#' mod2a.fit <- sem(mod2a, sample.cov=temp_mat, sample.nobs=500)
+#'
+#' # Run fit propensity analysis
+#' # Onion approach, save srmr and CFI
+#' res <- run.fitprop(mod1a.fit, mod2a.fit, fit.measure=c("srmr","cfi"),
+#'   rmethod="onion",reps=1000)
+#' summary(res)
+#'
+#' # Generate a variety of plots
+#' # ecdf - only srmr
+#' plot(res, whichfit = "srmr")
+#'
+#' # ecdf - both srmr and cfi, properly indicating that higher values of cfi are better
+#' plot(res, lower.tail=c(TRUE,FALSE))
+#'
+#' # ecdf change color palette, save plot to object, then display or change
+#' myplot<-plot(res, whichfit="cfi",
+#'              lower.tail=FALSE,
+#'              mod.brewer.pal="Dark2",
+#'              savePlot=TRUE)
+#' myplot[[1]] # saved to the first slot
+#' myplot[[1]] + theme_bw() # change something, like the theme
+#'
+#' # euler plot
+#' # cfi - which models have cfi of .5 or better?
+#' plot(res, type="euler", whichfit="cfi", lower.tail=FALSE, cutoff=.5)
+#'
+#' }
 #' @export
-#' @importFrom ggplot2 ggplot aes stat_ecdf scale_color_brewer theme element_blank xlab ylab xlim ylim
+#' @import ggplot2
 #' @importFrom graphics plot
 #' @importFrom stats na.omit
 #' @importFrom tidyr gather
@@ -234,13 +421,16 @@ plot.fitprop<-function(x,...,type=c("ecdf","euler","nVennR"),whichmod=NULL,which
         stat_ecdf(aes(linetype=.data$variable, color=.data$variable),na.rm=TRUE,size=.7) +
         scale_color_brewer(palette=mod.brewer.pal)
 
+      graph <- graph + guides(color=guide_legend(title="Model"))
+      graph <- graph + guides(linetype=guide_legend(title="Model"))
+
       if(lower.tail[m]){
         graph<-graph+ xlim(xlim[1],xlim[2])
       } else {
         graph<-graph+ xlim(xlim[2],xlim[1])
       }
-      graph<-graph+theme(legend.title=element_blank())+
-        xlab(fm) + ylab("Cumulative Probability")
+      graph<-graph+xlab(fm) + ylab("Cumulative Probability") #theme(legend.title=element_blank())+
+
     } else if (type=="euler"){
       if(lower.tail[m]){
         dat[,whichmod]<-dat[,whichmod]<cutoff # how many meet cutoff criterion
@@ -319,11 +509,46 @@ print.fitprop<-function(x,...){
 }
 
 #' Summary function for fitprop objects
-#' @param object Object of class fitprop, as created by run.fitprop function.
-#' @param ... Does nothing but to hopefully make this generic function pass R CMD check
-#' @param probs passed to quantile to determine what probabilities to report
-#' @param samereps Logical value indicating whether to use only results from replications in which all selected models yielded results
-#' @param lower.tail Logical vector indicating whether lower values of each fit index corresponds to good fit
+#' @param object Object of class fitprop, as created by \code{\link[ockhamSEM]{run.fitprop}} function.
+#' @param ... Does nothing but to hopefully make this generic function pass R CMD check.
+#' @param probs Vector passed to quantile to determine what probabilities to report.
+#' @param samereps Logical value indicating whether to use only results from replications in which all selected models yielded results.
+#' @param lower.tail Logical vector indicating whether lower values of each fit index corresponds to good fit.
+#' @examples
+#' \donttest{
+#'
+#' # Set up a covariance matrix to fit models to
+#' p<-3 # number of variables
+#' temp_mat <- diag(p) # identity matrix
+#' colnames(temp_mat) <- rownames(temp_mat) <- paste0("V", seq(1, p))
+#'
+#' # Define and fit two models using lavaan package
+#' mod1a <- 'V3 ~ V1 + V2
+#'   V1 ~~ 0*V2'
+#' mod2a <- 'V3 ~ V1
+#'   V2 ~ V3'
+#'
+#' mod1a.fit <- sem(mod1a, sample.cov=temp_mat, sample.nobs=500)
+#' mod2a.fit <- sem(mod2a, sample.cov=temp_mat, sample.nobs=500)
+#'
+#' # Run fit propensity analysis
+#' # Onion approach, save srmr and CFI
+#' res <- run.fitprop(mod1a.fit, mod2a.fit, fit.measure=c("srmr","cfi"),
+#'   rmethod="onion",reps=1000)
+#'
+#' # Generate summaries
+#' summary(res)
+#'
+#' # sort quantiles differently for srmr and cfi
+#' # Use different quantiles
+#' summary(res, probs = c(0,.25,.5,.75,1), lower.tail=c(TRUE,FALSE))
+#'
+#' # If some models failed to converge, this would result in
+#' # summaries computed on possibly different replications:
+#' # Use different quantiles
+#' summary(res, samereps=FALSE, lower.tail=c(TRUE,FALSE))
+#'
+#' }
 #' @export
 #' @importFrom stats quantile median ks.test na.omit
 #' @importFrom utils str
